0f3826cd509d529e4de888953c6ed67dc67a1884
Aryan Bhatt <aryanbhatt1002@gmail.com>
2021-11-18 20:01:49 +0530

    LinkedList

--- LinearLists/stacks/stack_LinkedList.cpp
+++ LinearLists/stacks/stack_LinkedList.cpp
@@ -1,4 +1,23 @@
 //
 // Created by aryan on 18-11-2021.
 //
+#include <iostream>
+using namespace std;
 
+class stackNode{
+public:
+    int data;
+    stackNode* next;
+};
+
+stackNode* newNode(int data){
+    stackNode* StackNode = new stackNode();
+    StackNode->data = data;
+    StackNode->next = nullptr;
+    return StackNode;
+}
+
+
+int main(){
+
+}
